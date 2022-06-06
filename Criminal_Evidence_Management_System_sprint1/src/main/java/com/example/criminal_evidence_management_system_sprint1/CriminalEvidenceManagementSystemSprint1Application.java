423964c720b9939d6d3ab09503648e81f20e4eef
Đặng Bảo QUốc <quocdb2@fpt.com.vn>
2022-06-06 11:11:11 +0700

    sprint1

---
+++ Criminal_Evidence_Management_System_sprint1/src/main/java/com/example/criminal_evidence_management_system_sprint1/CriminalEvidenceManagementSystemSprint1Application.java
@@ -0,0 +1,13 @@
+package com.example.criminal_evidence_management_system_sprint1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalEvidenceManagementSystemSprint1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriminalEvidenceManagementSystemSprint1Application.class, args);
+    }
+
+}
---
+++ Criminal_Evidence_Management_System_sprint1/src/test/java/com/example/criminal_evidence_management_system_sprint1/CriminalEvidenceManagementSystemSprint1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.criminal_evidence_management_system_sprint1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriminalEvidenceManagementSystemSprint1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
