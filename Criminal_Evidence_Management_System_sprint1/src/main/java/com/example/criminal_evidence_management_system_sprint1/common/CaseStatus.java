13c5492010a22afb1d9961ba6108b1b4d3c38636
Đặng Bảo QUốc <quocdb2@fpt.com.vn>
2022-06-09 08:42:22 +0700

    update spring 1 do up nham file

---
+++ Criminal_Evidence_Management_System_sprint1/src/main/java/com/example/criminal_evidence_management_system_sprint1/common/CaseStatus.java
@@ -0,0 +1,2 @@
+package com.example.criminal_evidence_management_system_sprint1.common;public enum CaseStatus {
+}
---
+++ Criminal_Evidence_Management_System_sprint2/src/main/java/com/example/criminal_evidence_management_system_sprint2/CriminalEvidenceManagementSystemSprint2Application.java
@@ -0,0 +1,13 @@
+package com.example.criminal_evidence_management_system_sprint2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalEvidenceManagementSystemSprint2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriminalEvidenceManagementSystemSprint2Application.class, args);
+    }
+
+}
