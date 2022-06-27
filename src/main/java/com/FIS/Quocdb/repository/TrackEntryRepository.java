ff3270ed565efe7eacd99341a04a6558b93546ea
Đặng Bảo Quốc <86763728+QuocDang0704@users.noreply.github.com>
2022-06-27 19:05:09 +0700

    Sprint3_done

--- src/main/java/com/FIS/Quocdb/repository/TrackEntryRepository.java
+++ src/main/java/com/FIS/Quocdb/repository/TrackEntryRepository.java
@@ -4,5 +4,4 @@ import com.FIS.Quocdb.entities.TrackEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrackEntryRepository extends JpaRepository<TrackEntry, Long> {
-
 }
